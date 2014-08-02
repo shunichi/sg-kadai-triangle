0aaac14f39afe7b1881f40c302e29e535c061fc2
Shunichi Ikegami <sike.tm@gmail.com>
2014-08-02 14:01:16 +0900

    Add examples.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,8 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
-  specify { expect(Triangle.check(2, 3, 4)).to eq '不等辺三角形ですね！' }
+  [2, 3, 4].permutation.each do |sides|
+    specify { expect(Triangle.check(*sides)).to eq '不等辺三角形ですね！' }
+  end
+
   specify { expect(Triangle.check(2, 2, 1)).to eq '二等辺三角形ですね！' }
   specify { expect(Triangle.check(1, 1, 1)).to eq '正三角形ですね！' }
-  specify { expect(Triangle.check(1, 2, 3)).to eq '三角形じゃないです＞＜' }
+
+  [1, 2, 3].permutation.each do |sides|
+    specify { expect(Triangle.check(*sides)).to eq '三角形じゃないです＞＜' }
+  end
+
+  [1, 2, 9].permutation.each do |sides|
+    specify { expect(Triangle.check(*sides)).to eq '三角形じゃないです＞＜' }
+  end
 end
